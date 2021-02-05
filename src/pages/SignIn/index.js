f21eca44f4cf5612457c6596d086c1a1569c223a
Paulo Rogerio <paulo-rpf@hotmail.com>
2021-02-05 19:12:23 -0400

    Creating the login page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
+import logo from '../../assets/logo.png';
 import Background from '../../components/Bakground';
-
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 const SignIn = () => (
   <Background>
-    <Text>SignIn</Text>
+    <Container>
+      <Image source={logo} />
+      <Form>
+        <FormInput
+          icon="mail-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Your email"
+        />
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Your password"
+        />
+        <SubmitButton onPress={() => {}}>Login</SubmitButton>
+      </Form>
+      <SignLink>
+        <SignLinkText>Create an account</SignLinkText>
+      </SignLink>
+    </Container>
   </Background>
 );
 
